ea3a3e9eeb99eda3cc5715bbab48b75d0bd4a824
Brett Bucsak <60727386+BBucsak@users.noreply.github.com>
2022-01-03 00:55:02 -0500

    Took nav function out of contact section

--- src/screens/contactMe/ContactMe.jsx
+++ src/screens/contactMe/ContactMe.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import './contactMe.css'
-import  { navigationLinks} from '../../helpers/navigationLinks'
 
-function createLinks() {
-  return navigationLinks.map((e, idx) => (
-    <p key={idx}><a href={e.ref}>{e.name}</a> </p>    
-  )
-  )
-}
+
 
 function ContactMe() {
   return (
