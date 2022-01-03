21e05d450d32cdbc1af7c15673f24daf28b8704c
Brett Bucsak <60727386+BBucsak@users.noreply.github.com>
2022-01-02 21:22:30 -0500

    2nd update

--- src/screens/aboutMe/AboutMe.js
+++ src/screens/aboutMe/AboutMe.js
@@ -10,9 +10,9 @@ function AboutMe() {
             </div>
             <div className="about_text">
                 <h1>ABOUT ME</h1>
-                <p> I am a freelance Front-End web developer based out of New Jersey here for your development needs. I am a veteran of the United States Airfoce where I served six years
-                    in the 108th Security Forces Squadran. My main goal is to give back to the community by helping citizens and other veterans grow succesful business's by creating web applications
-                    for them. When I am not working I enjoy traveling, photography, and spending time with family and friends. If you have a project you need please do not hesitate to reach out!
+                <p> I am a freelance Front-End web developer based out of New Jersey, here for your development needs. I am a veteran of the United States Air Force where I served six years
+                    in the 108th Security Forces Squadran. My main goal is to give back to the community by helping citizens and other veterans grow succesful businesses by creating web applications
+                    for them. When I am not working I enjoy traveling, photography, and spending time with family and friends. If you have a project you need, please do not hesitate to reach out!
                     No matter how big or small!  </p>
             </div>
         </div>
